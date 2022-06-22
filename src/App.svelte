fc8ed7b62a42004f348b7b65088577efe092c88e
OAALLTECHINFO <102165261+OAALLTECHINFO@users.noreply.github.com>
2022-06-22 19:04:34 +0400

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<style>
+	@import url("https://unpkg.com/fluent-svelte/theme.css");
+	:global(body) {
+		background-color: var(--fds-solid-background-base);
+		color: var(--fds-text-primary);
+	}
+</style>
+
+<script>
+	import { Router, Route, Link } from "svelte-navigator";
+	import { TextBox , Button } from "fluent-svelte";
+	import GradientText from 'svelte-gradient-typography';
+	let value = "";
+	import blog from "./blog.svelte";
+</script>
+
+<TextBox placeholder="Chose a Name" bind:value />
+
+<Button href="/blog" >
+	Blog
+</Button>
+
+<GradientText>
+    The {value}
+</GradientText>
+
+<Button href="https://www.youtube.com/channel/UCHKlIYMRw7XhpVhGmysPKDA" variant ="accent" >
+	Subscribe
+</Button>
---
+++ src/blog.svelte
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<h2>
+	Hello World and all
+</h2>
+
+<div class="blog">
+  <h2>blog</h2>
+</div>
